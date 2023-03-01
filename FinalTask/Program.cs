37e6db4c5899a11f9388c6e5d7df761a59ad4c08
WhiteBorshek <borshek98@gmail.com>
2023-03-01 14:36:49 +0500

    Написан код

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -4,3 +4,43 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+string[] GetData(string message)
+{
+    Console.Write($"{message}: ");
+    return Console.ReadLine()!.Split();
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write('[');
+    Console.Write(string.Join(", ", array));
+    Console.Write("]\n");
+}
+
+string[] MakeResultArray(string[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            counter += 1;
+    }
+    string[] resultArray = new string[counter];
+    counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultArray[counter] = array[i];
+            counter += 1;
+        }
+    }
+    return resultArray;
+}
+
+string[] array = GetData("Введите элементы массива, разделенные пробелом");
+Console.Write("Введенный Вами массив: ");
+PrintArray(array);
+string[] result = MakeResultArray(array);
+Console.Write("Массив из строк, длина которых <= 3: ");
+PrintArray(result);
